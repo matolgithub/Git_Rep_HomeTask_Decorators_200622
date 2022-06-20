1b30be02a595f75a710d3dc90fabfe5ce98a0d41
Олег Матшин <89175955650@mail.ru>
2022-06-20 18:52:31 +0300

    File home_task_logfile.log created.

--- HomeTask_Decorators_200622.py
+++ HomeTask_Decorators_200622.py
@@ -4,15 +4,23 @@ import datetime
 
 def log_file_maker(function):
     def _wrapper():
-        pass
+        data_list = []
+        data_list.append(datetime.date.today())
+        data_list.append(time.time())
+        data_list.append(f'{function}')
+        with open('home_task_logfile.log', 'w', encoding='utf-8') as file:
+            for data in data_list:
+                file.write(f'{data}\n')
+            print(data_list)
     return _wrapper
 
 
 @log_file_maker
-def simple_function():
-    print("Hello, World!")
+def simple_function(name='OLEG'):
+    simple_text = f'Have a nice day, {name}!'
+    print(simple_text)
+    return  simple_text
 
 
 if __name__ == '__main__':
-    # log_file_maker(simple_function)
     simple_function()
