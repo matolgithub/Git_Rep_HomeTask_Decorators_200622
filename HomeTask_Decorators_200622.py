5b45782033555d4d319ad93122a8d42fde666528
Олег Матшин <89175955650@mail.ru>
2022-06-20 22:54:54 +0300

    Task1 complete, write log-file.

--- HomeTask_Decorators_200622.py
+++ HomeTask_Decorators_200622.py
@@ -1,24 +1,23 @@
+import inspect
 import os
 import time
-import datetime
 
 def log_file_maker(function):
     def _wrapper():
         data_list = []
-        data_list.append(datetime.date.today())
-        data_list.append(time.time())
-        data_list.append(f'{function}')
+        data_list.append(time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()))
+        data_list.append(f'{function.__name__}')
+        data_list.append(inspect.signature(function))
+        data_list.append(f'{function()}')
         with open('home_task_logfile.log', 'w', encoding='utf-8') as file:
             for data in data_list:
                 file.write(f'{data}\n')
-            print(data_list)
     return _wrapper
 
 
 @log_file_maker
-def simple_function(name='OLEG'):
-    simple_text = f'Have a nice day, {name}!'
-    print(simple_text)
+def simple_function(name='Ivan', surname='Ivanov', specialty='student'):
+    simple_text = f'Hello, {specialty} {surname}! Have a nice day for you, {name}!'
     return  simple_text
 
 
